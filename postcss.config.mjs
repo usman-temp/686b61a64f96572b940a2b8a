ced31b969c73e9a6e161bd83f5f51bbb2912384a
usman-temp <usmanjaved1620@gmail.com>
2025-07-07 12:00:31 +0500

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// START EDIT
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
+// END EDIT
